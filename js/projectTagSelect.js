6e5aac359487b2547f304dcc6e8d1eab905ca2e6
Elias Neto <eliasneto321@outlook.com>
2022-10-16 00:40:59 -0300

    inseri projetos que fiz no explorer + fiz ajustes + desenvolvi js do projectTagSelect

---
+++ js/projectTagSelect.js
@@ -0,0 +1,48 @@
+function selectedProjectTag(projectTag) {
+  const projectTagA = document.querySelector('#projectsMain .projectTagA')
+  const projectTagB = document.querySelector('#projectsMain .projectTagB')
+
+  let selectedA
+  let selectedB
+
+  switch (projectTag) {
+    case 'a':
+      selectedA = true
+      selectedB = false
+      break
+    case 'b':
+      selectedA = false
+      selectedB = true
+      break
+    default:
+      break
+  }
+
+  let selected = [selectedA, selectedB]
+
+  let elements = [projectTagA, projectTagB]
+
+  for (const index in selected) {
+    if (selected[index]) {
+      elements[index].style.display = 'inherit'
+    } else {
+      elements[index].style.display = 'none'
+    }
+  }
+}
+
+function projectTagSelect() {
+  const projectsTags = document.querySelector('#projectsMain .projectsTags select')
+  let tagSelected = projectsTags.options[projectsTags.selectedIndex].value
+
+  switch (tagSelected) {
+    case 'a':
+      selectedProjectTag('a')
+      break
+    case 'b':
+      selectedProjectTag('b')
+      break
+    default:
+      break;
+  }
+}
